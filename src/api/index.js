b7a2cc69f1ac28aa2270088d2a793e75cf805275
Vince Aldrin Cabrera <vncealdcab@gmail.com>
2017-02-15 20:30:50 +0800

    hey

--- src/api/index.js
+++ src/api/index.js
@@ -13,7 +13,6 @@ export default ({ config, db }) => {
 	api.use('/todos', todos);
 	// todos.use('/:id/:field', (req, res) => res.send(req.params));
 
-
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
 		res.json({ version });
--- src/models/todo.js
+++ src/models/todo.js
@@ -2,9 +2,12 @@ import mongoose, { Schema } from 'mongoose';
 import mongoosePaginate from 'mongoose-paginate';
 
 const Todo = new Schema({
-	title: { type: String, default: '' },
-	description: { type: String, default: '' },
-	photoPath: { type: String, default: '' }
+	id: { type: String, default: '' },
+	name: { type: String, default: '' },
+	breed: { type: String, default: '' },
+	gender: { type: String, default: '' },
+	status: { type: String, default: '' },
+	suburb: { type: String, default: '' }
 }, { versionKey: false });
 
 Todo.plugin(mongoosePaginate);
